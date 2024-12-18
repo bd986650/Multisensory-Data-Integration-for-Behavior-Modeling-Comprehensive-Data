0571b8b72b7db92cb56b3d26731b8cf2cea053fa
Danil Belov <d.belov2@g.nsu.ru>
2024-12-18 09:33:31 +0700

    IOSapp

--- WebSite/life-cycles/src/Components/EntranceForm/ButtonAcceptForm/ButtonAcceptForm.js
+++
@@ -1,9 +0,0 @@
-import styles from './ButtonAcceptForm.module.css'
-
-export function ButtonAcceptForm({text, isDisabled, type}) {
-  return(
-    <button type={type} className={styles.button} disabled={isDisabled}>
-      <div className={styles.text}>{text}</div>
-    </button>
-  );
-}
--- WebSite/life-cycles/src/Components/WorkspaceMainPage/WorkspaceForGraph/Graph/Graph.js
+++
@@ -1,9 +0,0 @@
-import styles from './Graph.module.css'
-
-export function Graph() {
-  return(
-    <div className={styles.container}>
-      
-    </div>
-  );
-}
